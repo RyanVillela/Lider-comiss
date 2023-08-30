3c1a32a8ff0129558a94db600be6747ca84d79ea
RyanVillela <ryangvillela@gmail.com>
2023-08-30 16:44:23 -0300

    parte 02

--- vendcad.php
+++ vendcad.php
@@ -1,3 +1,21 @@
+<?php 
+$hostname = '127.0.0.1';
+$usurname = 'root';
+$password = '';
+$dataname = 'comissao_vendas';
+
+$conn = new mysqli($hostname, $usurname, $password, $dataname);
+
+if($conn->connect_error){
+  die("Erro na conexão: ".$conn->connect_errorone);
+}
+
+
+$sqli_select = "SELECT idvenda, 'data', usuario_id_usuario, fornecedor_cnpj FROM venda";
+$result = $conn->query($sqli_select);
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -27,6 +45,18 @@
               <td>Maria Anders</td>
               <td>Germany</td>
             </tr>
+            <?php
+              if($result->num_rows >0){
+                while($row = $result->fetch_assoc()){              
+                    echo("<tr>");
+                    echo("<td>".$row["idvenda"]."</td>");  
+                    echo("<td>".$row["data"]."</td>");
+                    echo("<td>".$row["usuario_id_usuario"]."</td>");
+                    echo("<td>".$row["fornecedor_cnpj"]."</td>");
+                    echo("<tr>"); 
+                }
+              }
+            ?>
             
           </table>
     
@@ -35,3 +65,7 @@
 </div>
 </body>
 </html>
+
+<?php
+$conn->close();
+?>

b2be8c803a2be21f7466654cd5a6d97286a114cc
RyanVillela <ryangvillela@gmail.com>
2023-08-30 09:45:15 -0300

    First

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div id="all_screen">
+    <div id="form_principal_login">
+        <div id="container_login">
+            <label for="username"><b>Razão</b>
+                <input style="margin: 0 0 10px 0;">
+            </label>
+            <label for="username"><b>Senha</b>
+                <input type="password">
+            </label>
+            <button type="submit">Login</button>
+        </div>
+        
+    </div>
+</div>
+</body>
+</html>
